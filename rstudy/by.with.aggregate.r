252b5d1501eff851e54846a500c585730366dab0
Huiming Song <huiming.song@wizecommerce.com>
2012-10-17 17:28:33 -0700

    20121017: test R functions like by, with, aggregate, sort ...

---
+++ rstudy/by.with.aggregate.r
@@ -0,0 +1,4 @@
+## na.string is: a character vector of strings which are to be interpreted as NA values
+
+data1=read.csv("D:\\hsong\\SkyDrive\\r_temp\\visitnew_summary.csv",header=T,na.strings=".")
+
