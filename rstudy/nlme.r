c690eca3199f7af13b8aba93ec7930566aaad605
Huiming Song <huiming.song@wizecommerce.com>
2012-11-18 17:40:49 -0800

    20121118: study of mixed model with library(nlme)

---
+++ rstudy/nlme.r
@@ -0,0 +1,112 @@
+# this is the codes from Linear Mixed Models by John Fox in 2002
+
+###### Part 1: Data and Data Overview
+
+### nlme study
+cat("\014")
+rm(list=ls())
+library(nlme)
+library(lattice)
+data(MathAchieve)
+head(MathAchieve)
+data(MathAchSchool)
+head(MathAchSchool)
+
+attach(MathAchieve)
+mses=tapply(SES, School, mean)
+head(mses)
+detach(MathAchieve)
+
+## create a dataframe called bryk
+bryk=as.data.frame(MathAchieve[,c("School","SES","MathAch")])
+names(bryk)<-tolower(names(bryk))
+head(bryk)
+sample20=sort(sample(7185,20))
+bryk[sample20,]
+
+bryk$meanses=mses[as.character(bryk$school)]  ## avoid using merge function
+bryk$cses=bryk$ses-bryk$meanses
+
+sector=MathAchSchool$Sector
+names(sector)<-row.names(MathAchSchool)
+bryk$sector=sector[as.character(bryk$school)]
+
+###  sample two schools from each sector to plot
+attach(bryk)
+cat<-sample(unique(school[sector=="Catholic"]), 20)
+cat.20<-groupedData(mathach~ses | school, data=bryk[is.element(school, cat),])
+
+pub<-sample(unique(school[sector=="Public"]), 20)
+pub.20<-groupedData(mathach~ses | school, data=bryk[is.element(school, pub),])
+
+trellis.device(color=T)
+# for Cathelic, the slopes are: some positive, some are almost 0, some are almost negative
+xyplot(mathach~ses | school, data=cat.20, main="Catholic",
+       panel=function(x,y){
+       	 panel.xyplot(x,y)
+	 panel.loess(x, y, span=1)
+	 panel.lmline(x, y, lty=2)
+	})
+
+# for Public, the slopes are: more positive than Cathelic
+xyplot(mathach~ses | school, data=pub.20, main="Public",
+       panel=function(x,y){
+         panel.xyplot(x,y)
+         panel.loess(x,y, span=1)
+      	 panel.lmline(x, y, lty=2)
+       })
+
+# linear regression for each school for each sector
+cat.list<-lmList(mathach~ses | school, subset=sector=="Catholic", data=bryk)
+pub.list<-lmList(mathach~ses | school, subset=sector=="Public", data=bryk)
+
+# plot of CI for intercepts and slopes in both regressions
+plot(intervals(cat.list), main="Catholic")
+plot(intervals(pub.list), main="Public")
+
+# To draw box plot to make it clear
+# overall, the Catholic school have a higher level of mathach while Public school have a higher average slope
+cat.coef=coef(cat.list)
+head(cat.coef)
+pub.coef=coef(pub.list)
+head(pub.coef)
+
+old<-par(mfrow=c(1,2))
+boxplot(cat.coef[,1], pub.coef[,1], main="Intercepts", names=c("Catholic", "Public"))
+boxplot(cat.coef[,2], pub.coef[,2], main="Slopes", names=c("Catholic", "Public"))
+par(old)
+
+######  Part 2: fit a hierarchical linear model with lme
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
