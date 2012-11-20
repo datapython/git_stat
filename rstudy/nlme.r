e9a1d98d470f73055984ee40c8375d0bd562ef82
huiming song <sasstudyblog@gmail.com>
2012-11-19 22:37:34 -0800

    20121109: study of lme for longitudinal data

--- rstudy/nlme.r
+++ rstudy/nlme.r
@@ -77,6 +77,24 @@ boxplot(cat.coef[,2], pub.coef[,2], main="Slopes", names=c("Catholic", "Public")
 par(old)
 
 ######  Part 2: fit a hierarchical linear model with lme
+bryk$sector=factor(bryk$sector, levels=c("Public", "Catholic"))
+contrasts(bryk$sector)
+
+lme(mathach ~ meanses*cses + sector*cses, random= ~ cses | school, data=bryk)->bryk.lme.1
+summary(bryk.lme.1)
+update(bryk.lme.1, random= ~1 | school)->bryk.lme.2  #omit random effects of cses
+summary(bryk.lme.2)
+anova(bryk.lme.1, bryk.lme.2)
+update(bryk.lme.1, random= ~cses - 1 | school)->bryk.lme.3  #omit random intetcepts
+summary(bryk.lme.3)
+anova(bryk.lme.3, bryk.lme.1)
+## There is strong evidence, then, that the average level of math achievement (as represented by the intercept) varies
+## from school to school, but not that the coefficient of SES varies, once differences between Catholic and public
+## schools are taken into account, and the average level of SES in the schools is held constant.
+
+
+#######  An example in Longitudinal Data
+library(car)
 
 
 
